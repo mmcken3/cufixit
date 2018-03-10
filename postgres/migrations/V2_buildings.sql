f007b650fc85e8e24ce4638fb36a15387eb1fa40
Hunter Bond <hunterbond@175.143.21.198.guest.wifi.dyn.clemson.edu>
2018-03-10 13:13:12 -0500

    Buildings imported, script started

---
+++ postgres/migrations/V2_buildings.sql
@@ -0,0 +1,21 @@
+INSERT INTO buildings (name) VALUES (
+'Hardin Hall',
+'Academic Success Center',
+'Barre Hall',
+'Biological Sciences Field Station',
+'Biosystems Research Complex',
+'Brackett Hall',
+'Brooks Center for the Performing Arts',
+'Cook Engineering Laboratory',
+'Cooper Library',
+'Daniel Hall',
+'Earle Hall',
+'Edwards Hall',
+'Endocrine Physiology Laboratory',
+'Fluor Daniel Engineering',
+'Innovation Building',
+'Freeman Hall',
+'Godfrey Hall',
+'Godley-Snell Research Center',
+'Greenhouse Complex'
+)
