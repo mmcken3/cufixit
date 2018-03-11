30774aaf14bc3453b2cc0e0bc7d48fbcf69e430d
Mitchell McKenzie <mmcken3@114.189.21.198.tigernet.wifi.dyn.clemson.edu>
2018-03-11 07:37:35 -0400

    update postgres

--- postgres/migrations/V2__buildings.sql
+++ postgres/migrations/V2__buildings.sql
@@ -1,136 +1,137 @@
-<Picker.Item label='Hardin Hall' value=label/>
-<Picker.Item label='Academic Success Center' value=label/>
-<Picker.Item label='Barre Hall' value=label/>
-<Picker.Item label='Biological Sciences Field Station' value=label/>
-<Picker.Item label='Biosystems Research Complex' value=label/>
-<Picker.Item label='Brackett Hall' value=label/>
-<Picker.Item label='Brooks Center for the Performing Arts' value=label/>
-<Picker.Item label='Cook Engineering Laboratory' value=label/>
-<Picker.Item label='Cooper Library' value=label/>
-<Picker.Item label='Daniel Hall' value=label/>
-<Picker.Item label='Earle Hall' value=label/>
-<Picker.Item label='Edwards Hall' value=label/>
-<Picker.Item label='Endocrine Physiology Laboratory' value=label/>
-<Picker.Item label='Fluor Daniel Engineering' value=label/>
-<Picker.Item label='Innovation Building' value=label/>
-<Picker.Item label='Freeman Hall' value=label/>
-<Picker.Item label='Godfrey Hall' value=label/>
-<Picker.Item label='Godley-Snell Research Center' value=label/>
-<Picker.Item label='Greenhouse Complex' value=label/>
-<Picker.Item label='Hardin Hall' value=label/>
-<Picker.Item label='Holtzendorff Hall' value=label/>
-<Picker.Item label='Houston Center' value=label/>
-<Picker.Item label='Hunter Chemistry Laboratory' value=label/>
-<Picker.Item label='Jordan Hall' value=label/>
-<Picker.Item label='Kinard Laboratory of Physics' value=label/>
-<Picker.Item label='Lee Hall' value=label/>
-<Picker.Item label='Lehotsky Hall' value=label/>
-<Picker.Item label='Life Sciences Building' value=label/>
-<Picker.Item label='Long Hall' value=label/>
-<Picker.Item label='Lowry Hall' value=label/>
-<Picker.Item label='Martin Hall' value=label/>
-<Picker.Item label='McAdams Hall' value=label/>
-<Picker.Item label='Newman Hall' value=label/>
-<Picker.Item label='Olin Hall' value=label/>
-<Picker.Item label='Poole Agricultural Center' value=label/>
-<Picker.Item label='Rhodes Annex' value=label/>
-<Picker.Item label='Rhodes Engineering Research Center' value=label/>
-<Picker.Item label='Riggs Hall' value=label/>
-<Picker.Item label='Sirrine Hall' value=label/>
-<Picker.Item label='Smith Building for Sonoco Institute of Packaging Design and Graphics' value=label/>
-<Picker.Item label='Strode Tower' value=label/>
-<Picker.Item label='Strom Thurmond Institute' value=label/>
-<Picker.Item label='Tillman Hall' value=label/>
-<Picker.Item label='Vickery Hall' value=label/>
-<Picker.Item label='Watt Family Innovation Center' value=label/>
-<Picker.Item label='Administrative Services Building' value=label/>
-<Picker.Item label='Alumni Center' value=label/>
-<Picker.Item label='Brackett Hall' value=label/>
-<Picker.Item label='Central Energy Facility' value=label/>
-<Picker.Item label='Clemson University Foundation/Shirley Center for Philanthropy' value=label/>
-<Picker.Item label='Dillard Building' value=label/>
-<Picker.Item label='Fire Station' value=label/>
-<Picker.Item label='Gentry Hall' value=label/>
-<Picker.Item label='Harcombe Hall' value=label/>
-<Picker.Item label='Kite Hill Recycling Center' value=label/>
-<Picker.Item label='Littlejohn House' value=label/>
-<Picker.Item label='Maintenance Stores' value=label/>
-<Picker.Item label='Mell Hall' value=label/>
-<Picker.Item label='Motor Pool' value=label/>
-<Picker.Item label='National Dropout Prevention Center' value=label/>
-<Picker.Item label='Parking and Transportation Services' value=label/>
-<Picker.Item label='Police Department' value=label/>
-<Picker.Item label='President’s Home' value=label/>
-<Picker.Item label='Sikes Hall' value=label/>
-<Picker.Item label='Trustee House' value=label/>
-<Picker.Item label='University Facilities Offices' value=label/>
-<Picker.Item label='Clemson Memorial Stadium and Frank Howard Field' value=label/>
-<Picker.Item label='Football Practice Facility' value=label/>
-<Picker.Item label='IPTAY/Ticket Office' value=label/>
-<Picker.Item label='Jervey Athletic Center' value=label/>
-<Picker.Item label='Littlejohn Coliseum' value=label/>
-<Picker.Item label='McFadden Building' value=label/>
-<Picker.Item label='Riggs Field <Picker.Item label=Soccer Stadium value=label' value=label/>
-<Picker.Item label='Sloan Tennis Center' value=label/>
-<Picker.Item label='Tiger Field <Picker.Item label=Kingsmore Baseball Stadium value=label' value=label/>
-<Picker.Item label='Cooper Library <Picker.Item label=Cooper Café value=label' value=label/>
-<Picker.Item label='Fernow Street Café <Picker.Item label=Chick-Fil-A® value=label' value=label/>
-<Picker.Item label='Hendrix Student Center <Picker.Item label=Hendrix Food Court/> Einstein Bros® and P.O.D. Market value=label' value=label/>
-<Picker.Item label='Madren Center <Picker.Item label=Solé on the Green Restaurant value=label' value=label/>
-<Picker.Item label='Schilletter Dining Hall <Picker.Item label=Wendy’s® value=label' value=label/>
-<Picker.Item label='Watt Café' value=label/>
-<Picker.Item label='Barnett Hall' value=label/>
-<Picker.Item label='Benet Hall' value=label/>
-<Picker.Item label='Bowen Hall' value=label/>
-<Picker.Item label='Bradley Hall' value=label/>
-<Picker.Item label='Byrnes Hall' value=label/>
-<Picker.Item label='Calhoun Courts Apartments' value=label/>
-<Picker.Item label='Cope Hall' value=label/>
-<Picker.Item label='Core Campus' value=label/>
-<Picker.Item label='Donaldson Hall' value=label/>
-<Picker.Item label='Geer Hall' value=label/>
-<Picker.Item label='Holmes Hall' value=label/>
-<Picker.Item label='Johnstone Hall' value=label/>
-<Picker.Item label='Lever Hall' value=label/>
-<Picker.Item label='Lightsey Bridge I Apartments' value=label/>
-<Picker.Item label='Lightsey Bridge II Apartments' value=label/>
-<Picker.Item label='Manning Hall' value=label/>
-<Picker.Item label='Mauldin Hall' value=label/>
-<Picker.Item label='McCabe Hall' value=label/>
-<Picker.Item label='Norris Hall' value=label/>
-<Picker.Item label='Sanders Hall' value=label/>
-<Picker.Item label='Simpson Hall North' value=label/>
-<Picker.Item label='Simpson Hall South' value=label/>
-<Picker.Item label='Smith Hall' value=label/>
-<Picker.Item label='Stadium Suites' value=label/>
-<Picker.Item label='Thornhill Village Apartments' value=label/>
-<Picker.Item label='Wannamaker Hall' value=label/>
-<Picker.Item label='Young Hall' value=label/>
-<Picker.Item label='Barnes Center' value=label/>
-<Picker.Item label='Fike Recreation Center' value=label/>
-<Picker.Item label='Hendrix Student Center' value=label/>
-<Picker.Item label='Redfern Health Center' value=label/>
-<Picker.Item label='Union Station' value=label/>
-<Picker.Item label='University Union/> Edgar Brown' value=label/>
-<Picker.Item label='Botanical Gardens' value=label/>
-<Picker.Item label='Brooks Center for the Performing Arts' value=label/>
-<Picker.Item label='Campbell Carriage House Coffee and Gift Shop' value=label/>
-<Picker.Item label='Campbell Geology Museum' value=label/>
-<Picker.Item label='Campbell Museum of Natural History' value=label/>
-<Picker.Item label='Carillon Garden' value=label/>
-<Picker.Item label='Class of 1957 Rotunda' value=label/>
-<Picker.Item label='Class of 1960 North Green' value=label/>
-<Picker.Item label='Clemson/> Thomas Green Statue <Picker.Item label=Gantt Circle value=label' value=label/>
-<Picker.Item label='Cox Plaza' value=label/>
-<Picker.Item label='Fort Hill <Picker.Item label=Calhoun and Clemson Mansion value=label' value=label/>
-<Picker.Item label='Fran Hanson Discovery Center' value=label/>
-<Picker.Item label='Lee Hall <Picker.Item label=Lee Art Gallery value=label' value=label/>
-<Picker.Item label='Madren Center' value=label/>
-<Picker.Item label='Martin Inn' value=label/>
-<Picker.Item label='Military Heritage Plaza' value=label/>
-<Picker.Item label='Outdoor Theatre <Picker.Item label=Amphitheater value=label' value=label/>
-<Picker.Item label='Reunion Square' value=label/>
-<Picker.Item label='Scroll of Honor Memorial' value=label/>
-<Picker.Item label='Visitors Center/> Class of 1944' value=label/>
-<Picker.Item label='Walker Golf Course Clubhouse' value=label/>
-<Picker.Item label='Woodland Cemeter' value=label
+INSERT INTO building (name) VALUES
+('Hardin Hall'),
+('Academic Success Center'),
+('Barre Hall'),
+('Biological Sciences Field Station'),
+('Biosystems Research Complex'),
+('Brackett Hall'),
+('Brooks Center for the Performing Arts'),
+('Cook Engineering Laboratory'),
+('Cooper Library'),
+('Daniel Hall'),
+('Earle Hall'),
+('Edwards Hall'),
+('Endocrine Physiology Laboratory'),
+('Fluor Daniel Engineering'),
+('Innovation Building'),
+('Freeman Hall'),
+('Godfrey Hall'),
+('Godley-Snell Research Center'),
+('Greenhouse Complex'),
+('Hardin Hall'),
+('Holtzendorff Hall'),
+('Houston Center'),
+('Hunter Chemistry Laboratory'),
+('Jordan Hall'),
+('Kinard Laboratory of Physics'),
+('Lee Hall'),
+('Lehotsky Hall'),
+('Life Sciences Building'),
+('Long Hall'),
+('Lowry Hall'),
+('Martin Hall'),
+('McAdams Hall'),
+('Newman Hall'),
+('Olin Hall'),
+('Poole Agricultural Center'),
+('Rhodes Annex'),
+('Rhodes Engineering Research Center'),
+('Riggs Hall'),
+('Sirrine Hall'),
+('Smith Building for Sonoco Institute of Packaging Design and Graphics'),
+('Strode Tower'),
+('Strom Thurmond Institute'),
+('Tillman Hall'),
+('Vickery Hall'),
+('Watt Family Innovation Center'),
+('Administrative Services Building'),
+('Alumni Center'),
+('Brackett Hall'),
+('Central Energy Facility'),
+('Clemson University Foundation/Shirley Center for Philanthropy'),
+('Dillard Building'),
+('Fire Station'),
+('Gentry Hall'),
+('Harcombe Hall'),
+('Kite Hill Recycling Center'),
+('Littlejohn House'),
+('Maintenance Stores'),
+('Mell Hall'),
+('Motor Pool'),
+('National Dropout Prevention Center'),
+('Parking and Transportation Services'),
+('Police Department'),
+('President’s Home'),
+('Sikes Hall'),
+('Trustee House'),
+('University Facilities Offices'),
+('Clemson Memorial Stadium and Frank Howard Field'),
+('Football Practice Facility'),
+('IPTAY/Ticket Office'),
+('Jervey Athletic Center'),
+('Littlejohn Coliseum'),
+('McFadden Building'),
+('Riggs Field (Soccer Stadium)'),
+('Sloan Tennis Center'),
+('Tiger Field (Kingsmore Baseball Stadium)'),
+('Cooper Library (Cooper Café)'),
+('Fernow Street Café (Chick-Fil-A®)'),
+('Hendrix Student Center (Hendrix Food Court, Einstein Bros® and P.O.D. Market)'),
+('Madren Center (Solé on the Green Restaurant)'),
+('Schilletter Dining Hall (Wendy’s®)'),
+('Watt Café'),
+('Barnett Hall'),
+('Benet Hall'),
+('Bowen Hall'),
+('Bradley Hall'),
+('Byrnes Hall'),
+('Calhoun Courts Apartments'),
+('Cope Hall'),
+('Core Campus'),
+('Donaldson Hall'),
+('Geer Hall'),
+('Holmes Hall'),
+('Johnstone Hall'),
+('Lever Hall'),
+('Lightsey Bridge I Apartments'),
+('Lightsey Bridge II Apartments'),
+('Manning Hall'),
+('Mauldin Hall'),
+('McCabe Hall'),
+('Norris Hall'),
+('Sanders Hall'),
+('Simpson Hall North'),
+('Simpson Hall South'),
+('Smith Hall'),
+('Stadium Suites'),
+('Thornhill Village Apartments'),
+('Wannamaker Hall'),
+('Young Hall'),
+('Barnes Center'),
+('Fike Recreation Center'),
+('Hendrix Student Center'),
+('Redfern Health Center'),
+('Union Station'),
+('University Union, Edgar Brown'),
+('Botanical Gardens'),
+('Brooks Center for the Performing Arts'),
+('Campbell Carriage House Coffee and Gift Shop'),
+('Campbell Geology Museum'),
+('Campbell Museum of Natural History'),
+('Carillon Garden'),
+('Class of 1957 Rotunda'),
+('Class of 1960 North Green'),
+('Clemson, Thomas Green Statue (Gantt Circle)'),
+('Cox Plaza'),
+('Fort Hill (Calhoun and Clemson Mansion)'),
+('Fran Hanson Discovery Center'),
+('Lee Hall (Lee Art Gallery)'),
+('Madren Center'),
+('Martin Inn'),
+('Military Heritage Plaza'),
+('Outdoor Theatre (Amphitheater)'),
+('Reunion Square'),
+('Scroll of Honor Memorial'),
+('Visitors Center, Class of 1944'),
+('Walker Golf Course Clubhouse'),
+('Woodland Cemeter')
