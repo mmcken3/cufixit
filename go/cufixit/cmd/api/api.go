4c4711fa54f51978fdf631d2f854e646c33795cc
Mitchell McKenzie <mmcken3@114.189.21.198.tigernet.wifi.dyn.clemson.edu>
2018-03-11 01:38:44 -0500

    added the rest of the API endpoints

--- go/cufixit/cmd/api/api.go
+++ go/cufixit/cmd/api/api.go
@@ -12,6 +12,9 @@ func main() {
 
 	router.HandleFunc("/", GetIndex).Methods("GET")
 	router.HandleFunc("/v1/getall", GetAllFeedback).Methods("GET")
+	router.HandleFunc("/v1/get/type/{type}", GetFeedbackofType).Methods("GET")
+	router.HandleFunc("/v1/get/building/{building}", GetFeedbackofBuilding).Methods("GET")
+	router.HandleFunc("/v1/get/user/{user}", GetFeedbackofUser).Methods("GET")
 	router.HandleFunc("/v1/submit", SubmitFixIt).Methods("POST")
 
 	log.Fatal(http.ListenAndServe(":8002", router))
